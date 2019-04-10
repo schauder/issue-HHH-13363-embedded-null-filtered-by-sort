0d410d48bf642070972baa6f3f92b9bed92c75dc
Jens Schauder <jschauder@pivotal.io>
2019-04-10 11:22:36 +0200

    Added slightly simplified test.

--- src/test/java/com/example/demo/ProductRepositoryTests.java
+++ src/test/java/com/example/demo/ProductRepositoryTests.java
@@ -8,6 +8,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import java.util.List;
 
@@ -32,6 +35,7 @@ public class ProductRepositoryTests {
 
     @Test
     public void sortingShouldReturnAllProducts() {
+
 		VendorAddress address = entityManager.merge(new VendorAddress("FooCity", "12345", "FooStreet", "1"));
 		Vendor vendor = entityManager.merge(new Vendor("ACME", address));
         entityManager.merge(new Product("FUBAR", new Product.VendorInfo(vendor, "Best buddy")));
@@ -39,5 +43,57 @@ public class ProductRepositoryTests {
 		List<Product> products = productRepository.findAll(Sort.by("vendorInfo.vendor.address.street"));
         assertEquals("Repository should return demo product", 1, products.size());
     }
+
+
+	@Test
+	public void sortingByEmptyNestedFieldsShouldReturnAllProductsPlainJpa() {
+
+		entityManager.persist(new Product("FUBAR", null));
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Product> query = cb.createQuery(Product.class);
+		Root<Product> root = query.from(Product.class);
+		query.select(root)
+				.orderBy(cb.asc(root.get("vendorInfo").get("vendor").get("address").get("street")));
+
+
+		List<Product> products = entityManager.createQuery(query).getResultList();
+		assertEquals("Repository should return demo product", 1, products.size());
+	}
+
+
+	@Test
+	public void sortingByEmptyNestedFieldsShouldReturnAllProductsPlainJpaShortend() {
+
+		entityManager.persist(new Product("FUBAR", null));
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Product> query = cb.createQuery(Product.class);
+		Root<Product> root = query.from(Product.class);
+		query.select(root)
+				.orderBy(cb.asc(root.get("vendorInfo").get("vendor").get("name")));
+
+
+		List<Product> products = entityManager.createQuery(query).getResultList();
+		assertEquals("Repository should return demo product", 1, products.size());
+	}
+
+	@Test
+	public void sortingShouldReturnAllProductsPlainJpa() {
+
+		VendorAddress address = entityManager.merge(new VendorAddress("FooCity", "12345", "FooStreet", "1"));
+		Vendor vendor = entityManager.merge(new Vendor("ACME", address));
+		entityManager.merge(new Product("FUBAR", new Product.VendorInfo(vendor, "Best buddy")));
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Product> query = cb.createQuery(Product.class);
+		Root<Product> root = query.from(Product.class);
+		query.select(root)
+				.orderBy(cb.asc(root.get("vendorInfo").get("vendor").get("address").get("street")));
+
+
+		List<Product> products = entityManager.createQuery(query).getResultList();
+		assertEquals("Repository should return demo product", 1, products.size());
+	}
 
 }
